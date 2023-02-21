647c93dd609b18e30d97f6929d7c0e72e8ee2156
yubinka <kayubin27@gmail.com>
2023-02-21 21:18:51 +0900

    first commit

---
+++ project19/src/Project19.java
@@ -0,0 +1,13 @@
+
+public class Project19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The biggest adventure you can ever take is");
+		show();
+		System.out.println("to live the life of your dreams.");
+	}
+	public static void show() {
+		System.out.println("*************************");
+	}
+}
